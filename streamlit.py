73378c541a80b6ed90d91a8164416e1d39dbb644
mdfwg <muhammaddafawisnugalih@mail.ugm.ac.id>
2025-04-08 17:20:48 +0700

    upload final code and presentation

---
+++ streamlit.py
@@ -0,0 +1,72 @@
+import streamlit as st
+import numpy as np
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.models import load_model
+import matplotlib.pyplot as plt
+from PIL import Image
+
+model = load_model('model\cnn_plant_disease_classification_model.h5')
+
+class_names = {
+    0: 'Apple___Apple_scab',
+    1: 'Apple___Black_rot',
+    2: 'Apple___Cedar_apple_rust',
+    3: 'Apple___healthy',
+    4: 'Blueberry___healthy',
+    5: 'Cherry_(including_sour)___Powdery_mildew',
+    6: 'Cherry_(including_sour)___healthy',
+    7: 'Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot',
+    8: 'Corn_(maize)___Common_rust_',
+    9: 'Corn_(maize)___Northern_Leaf_Blight',
+    10: 'Corn_(maize)___healthy',
+    11: 'Grape___Black_rot',
+    12: 'Grape___Esca_(Black_Measles)',
+    13: 'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)',
+    14: 'Grape___healthy',
+    15: 'Orange___Haunglongbing_(Citrus_greening)',
+    16: 'Peach___Bacterial_spot',
+    17: 'Peach___healthy',
+    18: 'Pepper,_bell___Bacterial_spot',
+    19: 'Pepper,_bell___healthy',
+    20: 'Potato___Early_blight',
+    21: 'Potato___Late_blight',
+    22: 'Potato___healthy',
+    23: 'Raspberry___healthy',
+    24: 'Soybean___healthy',
+    25: 'Squash___Powdery_mildew',
+    26: 'Strawberry___Leaf_scorch',
+    27: 'Strawberry___healthy',
+    28: 'Tomato___Bacterial_spot',
+    29: 'Tomato___Early_blight',
+    30: 'Tomato___Late_blight',
+    31: 'Tomato___Leaf_Mold',
+    32: 'Tomato___Septoria_leaf_spot',
+    33: 'Tomato___Spider_mites Two-spotted_spider_mite',
+    34: 'Tomato___Target_Spot',
+    35: 'Tomato___Tomato_Yellow_Leaf_Curl_Virus',
+    36: 'Tomato___Tomato_mosaic_virus',
+    37: 'Tomato___healthy'
+}
+
+st.title('Plant Disease Classification')
+
+uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+
+if uploaded_file is not None:
+    img = Image.open(uploaded_file)
+    img = img.resize((256, 256))
+    st.image(img, caption='Uploaded Image')
+
+    img = img.resize((256, 256))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = x / 255.0
+
+    classes = model.predict(x)
+    predicted_class_index = np.argmax(classes)
+
+    predicted_class_name = class_names[predicted_class_index]
+    confidence_percentage = 100 * np.max(classes)
+
+    st.write(f"Predicted class: {predicted_class_name}")
+    st.write(f"Confidence: {confidence_percentage:.2f}%")
